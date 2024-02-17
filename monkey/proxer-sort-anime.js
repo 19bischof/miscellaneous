1dd5d5cc8fb24b18f3c71b14b97600d7a2c3291b
Moritz Bischof <59830009+19bischof@users.noreply.github.com>
2024-02-17 20:08:39 +0100

    Update proxer-sort-anime.js

--- monkey/proxer-sort-anime.js
+++ monkey/proxer-sort-anime.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name        Sort Animes by reviews
 // @namespace   Violentmonkey Scripts
-// @match       https://proxer.me/season*
+// @match       https://proxer.me/*
 // @grant       none
+// @require https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
 // @version     1.0
 // @author      -
 // @description 15.1.2024, 00:18:38
@@ -12,6 +13,11 @@ const extractOrder = (node) => {
   const [rating, number] = node.querySelector('div:nth-child(4)').innerText.match(/[0-9.]+/g);
   return rating * number;
 }
-const parent = document.querySelector('#seasonEntries > div');
-const sorted = [...parent.children].sort((a,b) => extractOrder(b) - extractOrder(a));
-sorted.forEach(node => parent.appendChild(node))
+VM.observe(document.body, () => {
+  if (!location.href.startsWith('https://proxer.me/season')) return;
+  const parent = document.querySelector('#seasonEntries > div');
+  if (!parent) return;
+  const sorted = [...parent.children].sort((a,b) => extractOrder(b) - extractOrder(a));
+  sorted.forEach(node => parent.appendChild(node))
+  return true;
+});
